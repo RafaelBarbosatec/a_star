ea6c94873bed54aadd007bff7c75cb5a8fcac8d8
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-12-19 17:43:35 +0100

    feat!: Removed Flutter dependency, optimized code and added tests

--- lib/a_star_algorithm.dart
+++ lib/a_star_algorithm.dart
@@ -1,22 +1,9 @@
-library a_star_algorithm;
+import 'package:meta/meta.dart';
 
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
+typedef ValueChanged<T> = void Function(T value);
 
 /// Class responsible for calculating the best route using the A* algorithm.
 class AStar {
-  final int rows;
-  final int columns;
-  final Point<int> start;
-  final Point<int> end;
-  final List<Point<int>> barriers;
-  final bool withDiagonal;
-  List<Tile> _doneList = [];
-  List<Tile> _waitList = [];
-
-  late List<List<Tile>> grid;
-
   AStar({
     required this.rows,
     required this.columns,
@@ -24,12 +11,24 @@ class AStar {
     required this.end,
     required this.barriers,
     this.withDiagonal = true,
-  }) {
-    grid = _createGrid(rows, columns, barriers);
-  }
+  }) : grid = _createGrid(rows, columns, barriers);
 
-  /// Method that starts the search
-  Iterable<Point<int>> findThePath({ValueChanged<List<Point<int>>>? doneList}) {
+  final int rows;
+  final int columns;
+  final (int, int) start;
+  final (int, int) end;
+  final List<(int, int)> barriers;
+  final bool withDiagonal;
+
+  late final List<List<Tile>> grid;
+
+  final _doneList = <Tile>[];
+  final _waitList = <Tile>[];
+
+  /// Finds the shortest path between the [start] and [end] points.
+  Iterable<(int, int)> findThePath({
+    ValueChanged<List<(int, int)>>? doneList,
+  }) {
     _doneList.clear();
     _waitList.clear();
 
@@ -39,156 +38,114 @@ class AStar {
 
     _addNeighbors(grid);
 
-    Tile startTile = grid[start.x.toInt()][start.y.toInt()];
-    Tile endTile = grid[end.x.toInt()][end.y.toInt()];
-
-    Tile? winner = _getTileWinner(
-      startTile,
-      endTile,
-    );
-
-    List<Point<int>> path = [end];
+    final startTile = grid[start.y][start.x];
+    final endTile = grid[end.y][end.x];
+    final winner = _getTileWinner(startTile, endTile);
+    final path = [end];
 
     if (winner != null) {
-      Tile? tileAux = winner.parent;
-      for (int i = 0; i < winner.g - 1; i++) {
-        path.add(tileAux!.position);
-        tileAux = tileAux.parent;
-      }
+      var currentTile = winner.parent;
+      do {
+        path.add(currentTile!.position);
+        currentTile = currentTile.parent;
+      } while (currentTile != null);
     }
     path.add(start);
     doneList?.call(_doneList.map((e) => e.position).toList());
 
-    if (winner == null && !_isNeighbors(start, end)) {
+    if (winner == null && !start.isNeighbour(end, withDiagonal: withDiagonal)) {
       path.clear();
     }
     return path.reversed;
   }
 
   /// Method that create the grid
-  List<List<Tile>> _createGrid(
-      int rows, int columns, List<Point<int>> barriers) {
-    List<List<Tile>> grid = [];
-    List.generate(columns, (x) {
-      List<Tile> rowList = [];
-      List.generate(rows, (y) {
-        final offset = Point(x, y);
-        bool isBarrie = barriers.where((element) {
-          return element == offset;
-        }).isNotEmpty;
+  static List<List<Tile>> _createGrid(
+    int rows,
+    int columns,
+    List<(int, int)> barriers,
+  ) {
+    final grid = <List<Tile>>[];
+    for (var y = 0; y < rows; y++) {
+      final rowList = <Tile>[];
+      for (var x = 0; x < columns; x++) {
+        final offset = (x, y);
         rowList.add(
           Tile(
             offset,
             [],
-            isBarrier: isBarrie,
+            isBarrier: barriers.contains(offset),
           ),
         );
-      });
+      }
       grid.add(rowList);
-    });
+    }
     return grid;
   }
 
-  /// Adds neighbors to cells
+  /// Adds neighbors to tiles.
   void _addNeighbors(List<List<Tile>> grid) {
-    grid.forEach((_) {
-      _.forEach((element) {
-        int x = element.position.x;
-        int y = element.position.y;
+    for (var y = 0; y < grid.length; y++) {
+      for (var x = 0; x < grid[0].length; x++) {
+        final element = grid[y][x];
 
-        /// adds in top
-        if (y > 0) {
-          final t = grid[x][y - 1];
-          if (!t.isBarrier) {
-            element.neighbors.add(t);
-          }
-        }
-
-        /// adds in bottom
-        if (y < (grid.first.length - 1)) {
-          final t = grid[x][y + 1];
-          if (!t.isBarrier) {
-            element.neighbors.add(t);
-          }
-        }
-
-        /// adds in left
-        if (x > 0) {
-          final t = grid[x - 1][y];
-          if (!t.isBarrier) {
-            element.neighbors.add(t);
-          }
-        }
-
-        /// adds in right
-        if (x < (grid.length - 1)) {
-          final t = grid[x + 1][y];
-          if (!t.isBarrier) {
-            element.neighbors.add(t);
+        for (final direction in _directions) {
+          final newX = x + direction.x;
+          final newY = y + direction.y;
+          if (newX >= 0 &&
+              newX < grid.first.length &&
+              newY >= 0 &&
+              newY < grid.length) {
+            final t = grid[newY][newX];
+            if (!t.isBarrier) {
+              element.neighbors.add(t);
+            }
           }
         }
 
         if (withDiagonal) {
-          /// adds in top-left
-          if (y > 0 && x > 0) {
-            final top = grid[x][y - 1];
-            final left = grid[x - 1][y];
-            final t = grid[x - 1][y - 1];
-            if (!t.isBarrier && !left.isBarrier && !top.isBarrier) {
-              element.neighbors.add(t);
-            }
-          }
-
-          /// adds in top-right
-          if (y > 0 && x < (grid.length - 1)) {
-            final top = grid[x][y - 1];
-            final right = grid[x + 1][y];
-            final t = grid[x + 1][y - 1];
-            if (!t.isBarrier && !top.isBarrier && !right.isBarrier) {
-              element.neighbors.add(t);
-            }
-          }
-
-          /// adds in bottom-left
-          if (x > 0 && y < (grid.first.length - 1)) {
-            final bottom = grid[x][y + 1];
-            final left = grid[x - 1][y];
-            final t = grid[x - 1][y + 1];
-            if (!t.isBarrier && !bottom.isBarrier && !left.isBarrier) {
-              element.neighbors.add(t);
-            }
-          }
-
-          /// adds in bottom-right
-          if (x < (grid.length - 1) && y < (grid.first.length - 1)) {
-            final bottom = grid[x][y + 1];
-            final right = grid[x + 1][y];
-            final t = grid[x + 1][y + 1];
-            if (!t.isBarrier && !bottom.isBarrier && !right.isBarrier) {
-              element.neighbors.add(t);
+          for (final direction in _diagonalDirections) {
+            final newX = x + direction.x;
+            final newY = y + direction.y;
+            if (newX >= 0 &&
+                newX < grid.first.length &&
+                newY >= 0 &&
+                newY < grid.length) {
+              final t = grid[newY][newX];
+              final adjacent1 = grid[newY][x];
+              final adjacent2 = grid[y][newX];
+              if (!t.isBarrier &&
+                  !adjacent1.isBarrier &&
+                  !adjacent2.isBarrier) {
+                element.neighbors.add(t);
+              }
             }
           }
         }
-      });
-    });
+      }
+    }
   }
 
-  /// Method recursive that execute the A* algorithm
+  /// Recursive method that executes the A* algorithm.
   Tile? _getTileWinner(Tile current, Tile end) {
-    if (current == end) return current;
+    if (current == end) {
+      return current;
+    }
     _waitList.remove(current);
 
     current.neighbors.forEach((element) {
-      _analiseDistance(element, end, parent: current);
+      _analyzeDistance(element, end, parent: current);
     });
 
     _doneList.add(current);
 
-    _waitList.addAll(current.neighbors.where((element) {
-      return !_doneList.contains(element);
-    }));
+    _waitList.addAll(
+      current.neighbors.where((element) {
+        return !_doneList.contains(element);
+      }),
+    );
 
-    _waitList.sort((a, b) => a.f.compareTo(b.f));
+    _waitList.sort((a, b) => a.totalCost.compareTo(b.totalCost));
 
     for (final element in _waitList.toList()) {
       if (!_doneList.contains(element)) {
@@ -202,178 +159,96 @@ class AStar {
     return null;
   }
 
-  /// Calculates the distance g and h
-  void _analiseDistance(Tile current, Tile end, {Tile? parent}) {
+  /// Calculates the distance costFromStart and heuristicCostToEnd.
+  void _analyzeDistance(Tile current, Tile end, {Tile? parent}) {
     if (current.parent == null) {
       current.parent = parent;
-      current.g = (current.parent?.g ?? 0) + 1;
+      current.costFromStart = (current.parent?.costFromStart ?? 0) + 1;
 
-      current.h = _distance(current, end);
+      current.heuristicCostToEnd = _distance(current, end);
     }
   }
 
   /// Calculates the distance between two tiles.
   int _distance(Tile tile1, Tile tile2) {
-    int distX = (tile1.position.x - tile2.position.x).abs();
-    int distY = (tile1.position.y - tile2.position.y).abs();
-    return distX + distY;
+    return (tile1.position.x - tile2.position.x).abs() +
+        (tile1.position.y - tile2.position.y).abs();
   }
 
   /// Resume path
   /// Example:
   /// [(1,2),(1,3),(1,4),(1,5)] = [(1,2),(1,5)]
-  static List<Point<int>> resumePath(Iterable<Point<int>> path) {
-    List<Point<int>> newPath =
-        _resumeDirection(path, TypeResumeDirection.axisX);
-    newPath = _resumeDirection(newPath, TypeResumeDirection.axisY);
-    newPath = _resumeDirection(newPath, TypeResumeDirection.bottomLeft);
-    newPath = _resumeDirection(newPath, TypeResumeDirection.bottomRight);
-    newPath = _resumeDirection(newPath, TypeResumeDirection.topLeft);
-    newPath = _resumeDirection(newPath, TypeResumeDirection.topRight);
+  static List<(int, int)> simplifyPath(Iterable<(int, int)> path) {
+    var newPath = path.toList();
+    for (final type in TypeResumeDirection.values) {
+      newPath = simplifyDirection(newPath, type);
+    }
     return newPath;
   }
 
-  static List<Point<int>> _resumeDirection(
-    Iterable<Point<int>> path,
+  @visibleForTesting
+  static List<(int, int)> simplifyDirection(
+    List<(int, int)> path,
     TypeResumeDirection type,
   ) {
-    List<Point<int>> newPath = [];
-    List<List<Point<int>>> listOffset = [];
-    int indexList = -1;
-    int currentX = 0;
-    int currentY = 0;
+    if (path.isEmpty) {
+      return [];
+    }
 
-    path.forEach((element) {
-      final dxDiagonal = element.x;
-      final dyDiagonal = element.y;
+    final newPath = <(int, int)>[];
+    final segments = <List<(int, int)>>[];
+    var currentX = path.first.x;
+    var currentY = path.first.y;
 
-      switch (type) {
-        case TypeResumeDirection.axisX:
-          if (element.x == currentX && listOffset.isNotEmpty) {
-            listOffset[indexList].add(element);
-          } else {
-            listOffset.add([element]);
-            indexList++;
-          }
-          break;
-        case TypeResumeDirection.axisY:
-          if (element.y == currentY && listOffset.isNotEmpty) {
-            listOffset[indexList].add(element);
-          } else {
-            listOffset.add([element]);
-            indexList++;
-          }
-          break;
-        case TypeResumeDirection.topLeft:
-          final nextDxDiagonal = (currentX - 1).floor();
-          final nextDyDiagonal = (currentY - 1).floor();
-          if (dxDiagonal == nextDxDiagonal &&
-              dyDiagonal == nextDyDiagonal &&
-              listOffset.isNotEmpty) {
-            listOffset[indexList].add(element);
-          } else {
-            listOffset.add([element]);
-            indexList++;
-          }
-          break;
-        case TypeResumeDirection.bottomLeft:
-          final nextDxDiagonal = (currentX - 1).floor();
-          final nextDyDiagonal = (currentY + 1).floor();
-          if (dxDiagonal == nextDxDiagonal &&
-              dyDiagonal == nextDyDiagonal &&
-              listOffset.isNotEmpty) {
-            listOffset[indexList].add(element);
-          } else {
-            listOffset.add([element]);
-            indexList++;
-          }
-          break;
-        case TypeResumeDirection.topRight:
-          final nextDxDiagonal = (currentX + 1).floor();
-          final nextDyDiagonal = (currentY - 1).floor();
-          if (dxDiagonal == nextDxDiagonal &&
-              dyDiagonal == nextDyDiagonal &&
-              listOffset.isNotEmpty) {
-            listOffset[indexList].add(element);
-          } else {
-            listOffset.add([element]);
-            indexList++;
-          }
-          break;
-        case TypeResumeDirection.bottomRight:
-          final nextDxDiagonal = (currentX + 1).floor();
-          final nextDyDiagonal = (currentY + 1).floor();
-          if (dxDiagonal == nextDxDiagonal &&
-              dyDiagonal == nextDyDiagonal &&
-              listOffset.isNotEmpty) {
-            listOffset[indexList].add(element);
-          } else {
-            listOffset.add([element]);
-            indexList++;
-          }
-          break;
+    for (final element in path) {
+      final shouldAdd = switch (type) {
+        TypeResumeDirection.axisX => element.x == currentX,
+        TypeResumeDirection.axisY => element.y == currentY,
+        _ => (element.x - currentX).abs() == 1 &&
+            (element.y - currentY).abs() == 1,
+      };
+
+      if (shouldAdd && segments.isNotEmpty) {
+        segments.last.add(element);
+      } else {
+        segments.add([element]);
       }
 
       currentX = element.x;
       currentY = element.y;
-    });
+    }
 
-    listOffset.forEach((element) {
-      if (element.length > 1) {
-        newPath.add(element.first);
-        newPath.add(element.last);
-      } else {
-        newPath.add(element.first);
+    for (final segment in segments) {
+      newPath.add(segment.first);
+      if (segment.length > 1) {
+        newPath.add(segment.last);
       }
-    });
+    }
 
     return newPath;
   }
-
-  bool _isNeighbors(Point<int> start, Point<int> end) {
-    bool isNeighbor = false;
-
-    int startX = start.x;
-    int startY = start.y;
-    int endX = end.x;
-    int endY = end.y;
-
-    if (startX + 1 == endX && startY == endY || //right
-            startX - 1 == endX && startY == endY || //left
-            startX == endX && startY + 1 == endY || //bottom
-            startX == endX && startY - 1 == endY //top
-        ) {
-      isNeighbor = true;
-    }
-
-    if (withDiagonal) {
-      if (startX + 1 == endX && startY + 1 == endY || //bottom-right
-              startX - 1 == endX && startY - 1 == endY || //top-left
-              startX - 1 == endX && startY + 1 == endY || //bottom-left
-              startX + 1 == endX && startY - 1 == endY //top-right
-          ) {
-        isNeighbor = true;
-      }
-    }
-
-    return isNeighbor;
-  }
 }
 
-/// Class used to represent each cell
+/// Represents a tile/cell in the grid.
 class Tile {
-  final Point<int> position;
+  Tile(
+    this.position,
+    this.neighbors, {
+    this.parent,
+    this.isBarrier = false,
+  });
+
+  final (int, int) position;
   Tile? parent;
   final List<Tile> neighbors;
   final bool isBarrier;
-  int g = 0;
-  int h = 0;
+  int costFromStart = 0;
+  int heuristicCostToEnd = 0;
 
-  int get f => g + h;
-
-  Tile(this.position, this.neighbors, {this.parent, this.isBarrier = false});
+  int get totalCost => costFromStart + heuristicCostToEnd;
 }
 
+@visibleForTesting
 enum TypeResumeDirection {
   axisX,
   axisY,
@@ -382,3 +257,35 @@ enum TypeResumeDirection {
   topRight,
   bottomRight,
 }
+
+extension on (int, int) {
+  int get x => $1;
+  int get y => $2;
+
+  bool isNeighbour((int, int) other, {bool withDiagonal = true}) {
+    if (this == other) {
+      return false;
+    }
+
+    if (withDiagonal) {
+      return (x - other.x).abs() <= 1 && (y - other.y).abs() <= 1;
+    }
+
+    return (x - other.x).abs() <= 1 && y == other.y ||
+        x == other.x && (y - other.y).abs() <= 1;
+  }
+}
+
+const _directions = [
+  (0, -1), // top
+  (0, 1), // bottom
+  (-1, 0), // left
+  (1, 0), // right
+];
+
+const _diagonalDirections = [
+  (-1, -1), // top-left
+  (1, -1), // top-right
+  (-1, 1), // bottom-left
+  (1, 1), // bottom-right
+];
--- test/a_star_test.dart
+++ test/a_star_test.dart
@@ -1,1 +1,72 @@
-void main() {}
+import 'package:a_star_algorithm/a_star_algorithm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('simplifyPath', () {
+    test('should simplify a path with redundant points', () {
+      final path = [
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (1, 5),
+      ];
+      final expected = [
+        (1, 2),
+        (1, 5),
+      ];
+
+      final result = AStar.simplifyPath(path);
+      expect(result, equals(expected));
+    });
+
+    test('should return an empty list for an empty path', () {
+      final path = <(int, int)>[];
+      final expected = <(int, int)>[];
+
+      final result = AStar.simplifyPath(path);
+      expect(result, equals(expected));
+    });
+  });
+
+  group('_simplifyDirection', () {
+    test('should simplify a path in the X direction', () {
+      final path = [
+        (1, 2),
+        (1, 3),
+        (1, 4),
+        (1, 5),
+      ];
+      final expected = [
+        (1, 2),
+        (1, 5),
+      ];
+
+      final result = AStar.simplifyDirection(path, TypeResumeDirection.axisX);
+      expect(result, equals(expected));
+    });
+
+    test('should simplify a path in the Y direction', () {
+      final path = [
+        (2, 1),
+        (3, 1),
+        (4, 1),
+        (5, 1),
+      ];
+      final expected = [
+        (2, 1),
+        (5, 1),
+      ];
+
+      final result = AStar.simplifyDirection(path, TypeResumeDirection.axisY);
+      expect(result, equals(expected));
+    });
+
+    test('should return an empty list for an empty path', () {
+      final path = <(int, int)>[];
+      final expected = <(int, int)>[];
+
+      final result = AStar.simplifyDirection(path, TypeResumeDirection.axisX);
+      expect(result, equals(expected));
+    });
+  });
+}
